36888f64187d90d5d2eabd56483c70cdcee7bbef
shendrya <onemorning1379@gmail.com>
2022-02-04 15:34:48 +0200

    c

--- bot.py
+++ bot.py
@@ -1,9 +1,14 @@
+
 import telebot
-import config
+TOKEN = '5242981332:AAGsvhmn5KMoT-EP2Dl_xZPLmJAowaUV9TA' # bot token from @BotFather
 
-bot=telebot.Telebot(config.token)
+bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(commands=['start'])
+def start_message(message):
+  bot.send_message(message.chat.id,"Привет ✌️ ")
+
+@bot.message_handler(content_types = ['text'])
 def lalala(message):
     bot.send_message(message.chat.id, message.text)
 
--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-token = '5242981332:AAGsvhmn5KMoT-EP2Dl_xZPLmJAowaUV9TA' # bot token from @BotFather
+TOKEN = '5242981332:AAGsvhmn5KMoT-EP2Dl_xZPLmJAowaUV9TA' # bot token from @BotFather
