e6623e165d4c28ee1e697324a598008c061d964f
shendrya <onemorning1379@gmail.com>
2022-02-08 19:08:56 +0200

    Тарики

---
+++ main.py
@@ -0,0 +1,26 @@
+import config
+import telebot
+
+bot = telebot.TeleBot(config.TOKEN)
+
+#from telebot import types
+
+@bot.message_handler(commands = ['start'])
+def start_message(message):
+    bot.send_message(message.chat.id,'Привет ™')
+
+@bot.message_handler(content_types = 'text')
+def message_reply(message):
+    if message.text == "Курс":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+        btc = types.KeyboardButton("BTC")
+        markup.add(btc)
+        ton = types.KeyboardButton("TONCOIN")
+        markup.add(ton)
+        bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup = markup)
+    elif message.text == "BTC":
+        bot.send_message(message.chat.id,'44500')
+    elif message.text == "TONCOIN":
+        bot.send_message(message.chat.id,'3')
+
+bot.infinity_polling()
